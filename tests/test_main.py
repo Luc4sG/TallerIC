c78c960c4aac4c972479e181a660866a3403c27c
Lucas Gauna <lks.gauna@gmail.com>
2025-06-12 11:52:31 -0300

    test2 sonar should fail on duplication

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,10 +6,25 @@ from main import app
 client = TestClient(app)
 
 
-# def test_read_root():
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert "calculator" in response.json()["description"].lower()
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.status_code == 200
+    assert "calculator" in response.json()["description"].lower()
 
 
 def test_add():
