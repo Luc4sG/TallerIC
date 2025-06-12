8369fbd5846a58229c0fff11fea906a4f12b118b
Lucas Gauna <lks.gauna@gmail.com>
2025-06-12 11:45:50 -0300

    change: comment test to result in coverage fail

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,10 +6,10 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "calculator" in response.json()["description"].lower()
+# def test_read_root():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert "calculator" in response.json()["description"].lower()
 
 
 def test_add():
